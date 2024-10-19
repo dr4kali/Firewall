5493779f0d710bc60e4d30fd4152f84191ca8975
dr4kali <chathurangamsandeera@gmail.com>
2024-10-19 21:54:56 +0530

    minor adjustment

---
+++ etc/firewall/config.py
@@ -0,0 +1,14 @@
+# File paths
+VPN_CONFIG_PATH = "/home/dr4kali/Downloads/lab_dr4kali.ovpn"
+
+# Rate limiting
+RATE_LIMIT = 1000  # Increase if too strict
+TIME_FRAME = 1  # Adjust to monitor in longer intervals if necessary
+
+# Performance thresholds
+CPU_THRESHOLD = 20
+MEMORY_THRESHOLD = 20
+
+# Enable Threat Intelligence
+Enable_Threat_Intelligence = "yes"  # yes to enable or "no" to disable
+API_KEY = 'xxxxxxxxxxxxxxxxxxxxxx'
